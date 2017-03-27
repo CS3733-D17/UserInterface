ababff9e9ea2d46b3143a1e45a4b676201df34df
srilman <slgamerplus@gmail.com>
2017-03-27 16:26:00 -0400

    Added Library ControlsFX

--- src/com/slackers/inc/sample/Controller.java
+++ src/com/slackers/inc/sample/Controller.java
@@ -1,6 +1,9 @@
 package com.slackers.inc.sample;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 
 
 import java.net.URL;
@@ -8,9 +11,21 @@ import java.util.ResourceBundle;
 
 public class Controller implements Initializable{
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    @FXML ToggleGroup signType;
+    @FXML ToggleButton signUpButton;
+    @FXML ToggleButton logInButton;
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {}
+
+    @FXML
+    void hello(){
+        System.out.println("button1");
+    }
+
+    @FXML
+    void bye(){
+        System.out.println("button2");
     }
 
 }
