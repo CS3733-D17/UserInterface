3a5fc223d2cc84b56e3e657558287fb90ac1d6aa
srilman <slgamerplus@gmail.com>
2017-03-27 20:18:13 -0400

    Fixed Login Page and Implemented Switching

--- src/com/slackers/inc/sample/Controller.java
+++ src/com/slackers/inc/sample/Controller.java
@@ -2,30 +2,33 @@ package com.slackers.inc.sample;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ToggleButton;
-import javafx.scene.control.ToggleGroup;
-
+import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable{
 
-    @FXML ToggleGroup signType;
-    @FXML ToggleButton signUpButton;
-    @FXML ToggleButton logInButton;
+    @FXML AnchorPane signUpPane;
+    @FXML AnchorPane logInPane;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {}
 
     @FXML
-    void hello(){
-        System.out.println("button1");
+    void signUpClick(){
+        signUpPane.setVisible(true);
+        signUpPane.setManaged(true);
+        logInPane.setVisible(false);
+        logInPane.setManaged(false);
     }
 
     @FXML
-    void bye(){
-        System.out.println("button2");
+    void logInClick(){
+        signUpPane.setVisible(false);
+        signUpPane.setManaged(false);
+        logInPane.setVisible(true);
+        logInPane.setManaged(true);
     }
 
 }
