dbcd0d8158202c0720987526950f1abe8632e6b9
srilman <slgamerplus@gmail.com>
2017-03-29 18:11:34 -0400

    Merging stuff fixed

--- src/com/slackers/inc/sample/Controllers/ApplicationsController.java
+++ src/com/slackers/inc/sample/Controllers/ApplicationsController.java
@@ -1,9 +1,12 @@
 package com.slackers.inc.sample.Controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Accordion;
 import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.TitledPane;
@@ -11,7 +14,11 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -58,5 +65,17 @@ public class ApplicationsController implements Initializable{
         }*/
 
     }
+
+    @FXML
+    public void addApplication() throws IOException {
+        Parent newApp = FXMLLoader.load(getClass().getResource("../FXML/form.fxml"));
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setTitle("New Form");
+        stage.setScene(new Scene(newApp));
+        stage.show();
+
+    }
+
 
 }
