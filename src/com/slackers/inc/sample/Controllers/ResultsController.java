829bda2dce681abb4bef5843a15b9f13ca8d17ca
LEEDLE_MACHINE\Jason <jabel@wpi.edu>
2017-03-29 01:18:39 -0400

    Added more to search page Created Results page and controller

---
+++ src/com/slackers/inc/sample/Controllers/ResultsController.java
@@ -0,0 +1,24 @@
+package com.slackers.inc.sample.Controllers;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+/**
+ * Created by Jason on 3/28/2017.
+ */
+public class ResultsController {
+    /*
+    ObservableList<String> tempList = FXCollections.observableArrayList("Drink 1", "Drink 2", "Drink 3", "Drink 4");
+
+    @FXML
+    ListView drinkList;
+
+    @FXML
+    private void initialize(){
+        drinkList.setItems(tempList);
+
+    }
+    */
+}
